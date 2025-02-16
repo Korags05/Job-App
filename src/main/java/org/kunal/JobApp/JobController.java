7fdfc47522c828f99e15234607cb872959c901d0
Kunal Saha <kunalsaha1107@gmail.com>
2025-02-17 00:50:08 +0530

    feat - viewJobs

--- src/main/java/org/kunal/JobApp/JobController.java
+++ src/main/java/org/kunal/JobApp/JobController.java
@@ -5,9 +5,12 @@ import org.kunal.JobApp.repo.JobRepo;
 import org.kunal.JobApp.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class JobController {
@@ -29,6 +32,13 @@ public class JobController {
     public String handleForm(JobPost jobPost) {
         jobService.addJob(jobPost);
         return "success";
+    }
+
+    @GetMapping("viewalljobs")
+    public String viewJobs(Model model) {
+        List<JobPost> jobs = jobService.getAllJobs();
+        model.addAttribute("jobPosts", jobs);
+        return "viewalljobs";
     }
 
 }
--- src/main/java/org/kunal/JobApp/repo/JobRepo.java
+++ src/main/java/org/kunal/JobApp/repo/JobRepo.java
@@ -10,13 +10,7 @@ import java.util.List;
 @Repository
 public class JobRepo {
 
-    List<JobPost> jobs = new ArrayList<>(Arrays.asList(
-            new JobPost(1,
-                    "Java Developer",
-                    "Must have good experience in core Java and advanced Java",
-                    2,
-                    List.of("Core Java", "J2EE", "Spring Boot", "Hibernate"))
-    ));
+    List<JobPost> jobs = new ArrayList<>();
 
 
     public List<JobPost> getAllJobs() {
