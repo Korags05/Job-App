b77d9814498d3f3a4fae85524a8c91860457347c
Kunal Saha <kunalsaha1107@gmail.com>
2025-02-17 00:33:46 +0530

    feat - working with layers

--- src/main/java/org/kunal/JobApp/JobController.java
+++ src/main/java/org/kunal/JobApp/JobController.java
@@ -1,6 +1,9 @@
 package org.kunal.JobApp;
 
 import org.kunal.JobApp.model.JobPost;
+import org.kunal.JobApp.repo.JobRepo;
+import org.kunal.JobApp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JobController {
+
+    @Autowired
+    private JobService jobService;
 
     @GetMapping({"/", "home"})
     public String home() {
@@ -21,6 +27,7 @@ public class JobController {
 
     @PostMapping("handleForm")
     public String handleForm(JobPost jobPost) {
+        jobService.addJob(jobPost);
         return "success";
     }
 
--- src/main/java/org/kunal/JobApp/model/JobPost.java
+++ src/main/java/org/kunal/JobApp/model/JobPost.java
@@ -3,14 +3,11 @@ package org.kunal.JobApp.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
-
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Component
 public class JobPost {
     private int postId;
     private String postProfile;
